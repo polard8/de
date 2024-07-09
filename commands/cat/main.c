32feb1d15b3932fb7e425bfc53a5089f54e542ae
Fred Nora <freddynora@gmail.com>
2024-07-09 09:04:04 -0300

    commands: Improving the cat command.

--- commands/cat/main.c
+++ commands/cat/main.c
@@ -1,4 +1,4 @@
-// cat - concatenate files and print on the standard output
+// cat - Concatenate files and print on the standard output
 // 2022 - Fred Nora.
 // See:
 // https://man7.org/linux/man-pages/man1/cat.1.html
@@ -20,6 +20,7 @@
 
 //4KB
 #define __BufferSize  (4*1024)
+static char buffer[__BufferSize];
 
 const char *VersionString = "Version 1.0";
 const char *HelpString = "options: --help, --version, --number";
@@ -29,6 +30,7 @@ const char *HelpString = "options: --help, --version, --number";
 static void doHelp(void);
 static void doVersion(void);
 // ...
+static void __clear_buffer(void);
 //-----------------------------
 
 static void doHelp(void)
@@ -40,11 +42,16 @@ static void doVersion(void)
     printf("%s\n",VersionString);
 }
 
+static void __clear_buffer(void)
+{
+    register int i=0;
+    for (i=0; i<__BufferSize; i++)
+        buffer[i] = 0;
+}
+
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    static char buffer[__BufferSize];  // tmp
-
     int fd=-1;
     register int nreads = 0;
     register int nwrites = 0;
@@ -65,60 +72,53 @@ int main(int argc, char *argv[])
     printf("\n");
     */
 
-    if (argc <= 0){
-        printf("cat: No args\n");
+    if (argc <= 1){
+        printf("Few parameters\n");
+	doHelp();
         goto fail;
     }
-    if (argc == 1){
-        printf("cat: We need more args\n");
-        //call usage()
-        goto fail;
+    if (argc >= Max){
+        printf("Too many files\n");
+	goto fail;
     }
 
 // Clear the tmp buffer.
-// #todo: Actually we're gonna malloc the buffer
+// #todo: 
+// Actually we're gonna malloc the buffer
 // based on the file size. I guess.
-
-    for (i=0; i<__BufferSize; i++){
-        buffer[i] = 0;
-    };
-
-    if (argc > Max){
-        printf("Too much files in the commnad line\n");
-        goto fail;
-    }
+    __clear_buffer();
 
 // Probe for some flags.
-    int isFlag=FALSE;
+    int isFlag = FALSE;
     for (i=1; i<argc; i++)
     {
-        isFlag=FALSE;
+        isFlag = FALSE;
 
         if ( strncmp( argv[i], "--help", 6) == 0 ){
-            doHelp();
             isFlag=TRUE;
+	    doHelp();
             goto done;
         }
         if ( strncmp( argv[i], "--version", 9) == 0 ){
-            doVersion();
             isFlag=TRUE;
+	    doVersion();
             goto done;
         }
         if ( strncmp( argv[i], "--number", 8) == 0 ){
-            fNumber = TRUE;
             isFlag=TRUE;
+	    fNumber = TRUE;
         }
         if ( strncmp( argv[i], "--show-tabs", 11) == 0 ){
-            fShowTabs = TRUE;
-            isFlag=TRUE;
+            isFlag = TRUE;
+	    fShowTabs = TRUE;
         }
         if ( strncmp( argv[i], "--show-ends", 11) == 0 ){
-            fShowEnds = TRUE;
-            isFlag=TRUE;
+            isFlag = TRUE;
+	    fShowEnds = TRUE;
         }
         
         //
-        // It's not a flag.
+        // It's NOT a flag.
         //
 
         // Open the file and print the content into the screen.
@@ -134,12 +134,15 @@ int main(int argc, char *argv[])
                 printf ("cat: read() failed\n");
                 goto fail;
             }
+	    // Print the file into the screen.
             // Write on stdout.
             nwrites = write( 1, buffer, sizeof(buffer) );
             if (nwrites <= 0){
                 printf ("cat: File {%d} failed on write()\n", i);
                 goto fail;
             }
+	    // Clear the buffer
+	    __clear_buffer();
         }
     };
 
